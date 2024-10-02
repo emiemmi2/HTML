ecfb9d6a11a29149e2e67aabffc634110ac2ea10
emilia leppanen <emilia.leppanen@edu.keuda.fi>
2024-10-02 12:32:30 +0300

    eka

---
+++ tehtäviä.js
@@ -0,0 +1,41 @@
+console.log("Hei maailma");
+// tehtava 1
+var luku1, luku2, summa;
+luku1 = 15;
+luku2 = 10;
+summa = luku1 + luku2;
+console.log(summa);
+//tehtava2
+luku1 = 10;
+luku2 = 6;
+var erotus = luku1 - luku2;
+console.log(erotus);
+//tehtava3
+luku1 = 8;
+luku2 = 4;
+var tulo = luku1 * luku2;
+console.log(tulo)
+//tehtava4
+luku1 = 6;
+luku2 = 2;
+var osamaara = luku1 / luku2;
+console.log(osamaara)
+//tehtava5
+var Celsius, Fahrenheit;
+Celsius = 45;
+Fahrenheit = Celsius * 9 / 5 + 32;
+console.log(Fahrenheit);
+//tehtava6
+Fahrenheit = 44;
+Celsius = (Fahrenheit - 32) * 5 / 9;
+console.log(Celsius);
+//tehtava7
+var km, malli, muunnos = 1.406;
+malli = 100;
+km = muunnos * malli;
+console.log(km);
+//tehtava8
+var km, malli, muunnos = 1.609
+malli = 100;
+km = muunnos * malli;
+console.log(km);
